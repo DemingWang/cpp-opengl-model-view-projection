ffe5052529b4ff1743c6e07fea54a54a7ee66320
afiskon <afiskon@gmail.com>
2015-09-15 19:15:30 +0300

    Fixes for MacOS

--- main.cpp
+++ main.cpp
@@ -106,6 +106,7 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
   GLFWwindow* window = glfwCreateWindow(800, 600, "Rotating cube", nullptr, nullptr);
   if(window == nullptr) {
